ddb1a125570daa4f84bc983afdd3768419d7cb28
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2017-06-20 18:34:54 -0400

    update

--- site-cookbooks/corista-openstack/metadata.rb
+++ site-cookbooks/corista-openstack/metadata.rb
@@ -5,4 +5,5 @@ description 'Helper cookbook openstack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 
-depends 'openstack-common', '>= 15.0.0'
+depends 'openstack-common'
+depends 'openstack-compute'
