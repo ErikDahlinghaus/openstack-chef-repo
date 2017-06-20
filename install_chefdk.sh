401c6fbe0af892e3d32f0fe19a9ec0e31e8de655
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2017-06-20 06:13:12 -0400

    updates install_chefdk

--- install_chefdk.sh
+++ install_chefdk.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo -s
+# root check
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+# upgrade system
 apt update && apt -y dist-upgrade
+
+# download chefdk
 cd /tmp
-wget https://packages.chef.io/files/stable/chefdk/1.2.22/ubuntu/16.04/chefdk_1.2.22-1_amd64.deb
-dpkg -i chefdk*.deb
+DEB_PACKAGE="chefdk_1.2.22-1_amd64.deb"
+if [ -f "$DEB_PACKAGE" ]; then
+  rm "$DEB_PACKAGE"
+fi
+wget "https://packages.chef.io/files/stable/chefdk/1.2.22/ubuntu/16.04/$DEB_PACKAGE"
+
+# install chefdk
+dpkg -i "$DEB_PACKAGE"
 apt install -f
