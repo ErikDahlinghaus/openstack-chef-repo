11e77c101aad38f3a14fbe7c234a7d8b066ae241
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2017-06-20 18:46:55 -0400

    fixes asset collection

--- site-cookbooks/corista-openstack/recipes/compile_dashboard_assets.rb
+++ site-cookbooks/corista-openstack/recipes/compile_dashboard_assets.rb
@@ -1,4 +1,13 @@
-execute 'compile dashboard assets' do
+execute 'collect assets' do
+  user 'root'
   cwd '/usr/share/openstack-dashboard'
-  command './manage.py collectstatic && ./manage.py compress --force'
+  command './manage.py collectstatic'
+  not_if 'test -d /var/lib/openstack-dashboard/static/dashboard/'
+end
+
+execute 'compress assets' do
+  user 'root'
+  cwd '/usr/share/openstack-dashboard'
+  command './manage.py compress --force'
+  not_if 'test -d /usr/share/openstack-dashboard/openstack_dashboard/templatetags/'
 end
