2a6fd2e1321a28d3f1aed765b13c44b8bc3955d6
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2017-06-27 15:30:24 -0400

    networking

--- site-cookbooks/corista-openstack/recipes/install_memcached.rb
+++ site-cookbooks/corista-openstack/recipes/install_memcached.rb
@@ -6,7 +6,6 @@ ip = node['corista-openstack']['controller_host']
 
 ruby_block 'configure memcached' do
   block do
-    # no-op
     fe = Chef::Util::FileEdit.new("/etc/memcached.conf")
     fe.search_file_replace_line('-l 127.0.0.1', "-l #{ip}")
     fe.write_file
