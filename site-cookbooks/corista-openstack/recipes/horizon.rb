53490fa3d405a17f14604e930eb2597bf6c95eec
Erik Dahlinghaus <erik.dahlinghaus@gmail.com>
2017-06-27 18:17:57 -0400

    dashboard

--- site-cookbooks/corista-openstack/recipes/horizon.rb
+++ site-cookbooks/corista-openstack/recipes/horizon.rb
@@ -1,22 +1,12 @@
 include_recipe 'openstack-dashboard::horizon'
 
-require 'pp'
-
 local_settings_path = node['openstack']['dashboard']['local_settings_path']
-local_settings_template = nil
 begin
+  # Use our cookbook's version of the local_settings.py.erb
   local_settings_template = resources(template: local_settings_path)
   local_settings_template.cookbook('corista-openstack')
 rescue Chef::Exceptions::ResourceNotFound => e
-  puts "\n\n\n\n"
-  puts "RESOURCE NOT FOUND template[#{local_settings_path}]"
-  puts "\n\n\n\n"
-  pp e
-  puts "\n\n\n\n"
+  Chef::Log.error "RESOURCE NOT FOUND template[#{local_settings_path}]"
 end
-
-puts "\n\n\n\n"
-pp "#{local_settings_template.variables}"
-puts "\n\n\n\n"
 
 include_recipe 'corista-openstack::compile_dashboard_assets'
